786e0c5a3a3a1560bb08e218454b839ee964d42b
AlvaroPeris <alvarop@mail.com>
2024-02-18 04:05:46 +0100

    correcciones

--- Writters/mqcli/example.sh
+++ Writters/mqcli/example.sh
@@ -2,6 +2,6 @@ export MQ_PROPERTIES="/home/ubuntu23/tmpprojects/was-ejb/Writters/mqcli/cliprope
 messages=50
 
 ./mqcli.sh putjms --config "$MQ_PROPERTIES" --queue "CANAL.INTERNET" --message "Mensaje aleatorio $(date)" &
-./mqcli.sh putrandomimportes --config "$MQ_PROPERTIES" --queue "CANAL.INTERNET" --channel "internet" --number $messages &
-./mqcli.sh putrandomimportes --config "$MQ_PROPERTIES" --queue "CANAL.TIENDA" --channel "tienda" --number $messages &
-./mqcli.sh putrandomimportes --config "$MQ_PROPERTIES" --queue "CANAL.TELEFONICO" --channel "telefonico" --number $messages
+./mqcli.sh putrandomimportes --config "$MQ_PROPERTIES" --queue "CANAL.INTERNET" --channel "internet" --number 10 &
+./mqcli.sh putrandomimportes --config "$MQ_PROPERTIES" --queue "CANAL.TIENDA" --channel "tienda" --number 20 &
+./mqcli.sh putrandomimportes --config "$MQ_PROPERTIES" --queue "CANAL.TELEFONICO" --channel "telefonico" --number 5
